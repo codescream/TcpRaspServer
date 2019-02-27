979f3eff9a4a29339490c517eb867e39d258a51c
Mark Ogilo <44981320+codescream@users.noreply.github.com>
2019-02-27 11:54:10 -0500

    clean up

--- TcpRaspServer/Program.cs
+++ TcpRaspServer/Program.cs
@@ -18,7 +18,7 @@ namespace DemoHarnessUpd
     {
        // public static GpioPin led = new GpioPin(17, Direction.Out);
         public static String[] ctrl_cmd = {"forward", "backward", "left", "right", "stop", "read cpu_temp", "home", "distance", "x+",
-            "x-", "y+", "y-", "xy_home", "Speak", "Image", "Voice", "PlayB", "StopR", "AFred", "FredS", "Reco1", "Reco2", "ConfE", "CancE", "UpdKB", "GetEn", "DelPr"};
+            "x-", "y+", "y-", "xy_home", "Speak", "Image", "Record", "PlayB", "StopR", "AFred", "FredS", "Reco1", "Reco2", "ConfE", "CancE", "UpdKB", "GetEn", "DelPr"};
 
         public static string text = "";
         static TextToSpeech tts = new TextToSpeech();
@@ -57,7 +57,7 @@ namespace DemoHarnessUpd
 
                 // Buffer for reading data
                 Byte[] bytes = new Byte[1024];
-                String data = null;
+                String dataReceived = null;
 
                 // Enter the listening loop.
                 while (true)
@@ -69,8 +69,6 @@ namespace DemoHarnessUpd
                     TcpClient client = server.AcceptTcpClient();
                     Console.WriteLine("Connected!");
 
-                    data = null;
-
                     // Get a stream object for reading and writing
                     NetworkStream stream = client.GetStream();
 
@@ -79,25 +77,27 @@ namespace DemoHarnessUpd
                     // Loop to receive all the data sent by the client.
                     //while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                     //{
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
+                    // Translate data bytes to a ASCII string.
+                    dataReceived = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", dataReceived);
 
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-                        string test = "";
+                    // Process the data sent by the client.
+                    /*data = data.ToUpper();
+                    string test = "";
 
-                        int dataLen = data.Length - 5;
-                        if(dataLen > 0)
-                        {
-                            test = data.Substring(dataLen);
-                        }
+                    int dataLen = data.Length - 5;
+                    if(dataLen > 0)
+                    {
+                        test = data.Substring(dataLen);
+                    }*/
+
+                    string[] data = dataReceived.Split('-');
                         
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(dataReceived);
 
                         // Send back a response.
                         //stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
+                        Console.WriteLine("Sent: {0}", dataReceived);
                     //}
 
                     //Console.WriteLine("Done!");
@@ -107,98 +107,98 @@ namespace DemoHarnessUpd
 
                     Console.WriteLine(data);
 
-                   if(data == ctrl_cmd[0])
+                   if(data[0] == ctrl_cmd[0])
                     {
                         //move forward
                         GpioPin fwd = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         fwd.Value = PinValue.High;
                     }
-                   else if(data == ctrl_cmd[1])
+                   else if(data[0] == ctrl_cmd[1])
                     {
                         //move backward
                         GpioPin bkwd = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         bkwd.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[2])
+                    else if (data[0] == ctrl_cmd[2])
                     {
                         //move left
                         GpioPin lft = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         lft.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[3])
+                    else if (data[0] == ctrl_cmd[3])
                     {
                         //move right
                         GpioPin rgt = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         rgt.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[4])
+                    else if (data[0] == ctrl_cmd[4])
                     {
                         //stop
                         GpioPin stp = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         stp.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[5])
+                    else if (data[0] == ctrl_cmd[5])
                     {
                         //read cpu_temp
                     }
-                    else if (data == ctrl_cmd[6])
+                    else if (data[0] == ctrl_cmd[6])
                     {
                         //home
                         GpioPin hme = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         hme.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[7])
+                    else if (data[0] == ctrl_cmd[7])
                     {
                         //distance
                     }
-                    else if (data == ctrl_cmd[8])
+                    else if (data[0] == ctrl_cmd[8])
                     {
                         //x+
                         GpioPin xup = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         xup.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[9])
+                    else if (data[0] == ctrl_cmd[9])
                     {
                         //x-
                         GpioPin xdn = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         xdn.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[10])
+                    else if (data[0] == ctrl_cmd[10])
                     {
                         //y+
                         GpioPin yup = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         yup.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[11])
+                    else if (data[0] == ctrl_cmd[11])
                     {
                         //y-
                         GpioPin ydn = new GpioPin(17, Direction.Out);//change 17 to the actual pin #
                         ydn.Value = PinValue.High;
                     }
-                    else if (data == ctrl_cmd[12])
+                    else if (data[0] == ctrl_cmd[12])
                     {
                         //xy_home
                     }
-                    else if (test == ctrl_cmd[13].ToUpper()) // Speak
+                    else if (data[0] == ctrl_cmd[13]) // Speak
                     {
-                        Console.WriteLine(data.Remove(dataLen));
-                        tts.TextToWords(data.Remove(dataLen)).Wait();
+                        Console.WriteLine(data[1]);
+                        tts.TextToWords(data[1]).Wait();
                     }
-                    else if (data == ctrl_cmd[14].ToUpper()) // Image and Read-CV
+                    else if (data[0] == ctrl_cmd[14]) // Image and Read-CV
                     {
-                        Console.WriteLine(data.Remove(dataLen));
+                        Console.WriteLine(data[1]);
                         CompVision imageCV = new CompVision();
 
                         string text = imageCV.AnalyzeImage();
                         tts.TextToWords(text).Wait();
                     }
-                    else if (data == ctrl_cmd[15].ToUpper()) // Voice - Record
+                    else if (data[0] == ctrl_cmd[15]) // Voice - Record
                     {
                         VarHolder.LinuxRecTime = "";
-                        Console.WriteLine(data.Remove(dataLen));
+                        Console.WriteLine(data[1]);
                         player.Record().Wait();
                     }
-                    else if (data == ctrl_cmd[16].ToUpper()) // PlayBack recording
+                    else if (data[0] == ctrl_cmd[16]) // PlayBack recording
                     {
                         string path = Directory.GetCurrentDirectory();
                         if(path.Contains("\\"))
@@ -209,21 +209,21 @@ namespace DemoHarnessUpd
                         {
                             path += "/record.wav";
                         }
-                        Console.WriteLine(data.Remove(dataLen));
+                        Console.WriteLine(data[1]);
                         player.Play(path).Wait();
                     }
-                    else if (data == ctrl_cmd[17].ToUpper()) // Stop recording
+                    else if (data[0] == ctrl_cmd[17]) // Stop recording
                     {
                         VarHolder.LinuxRecTime = "";
                         player.StopRecording().Wait();
                     }
-                    else if (data == ctrl_cmd[18].ToUpper()) // Ask Fred - QnA
+                    else if (data[0] == ctrl_cmd[18]) // Ask Fred - QnA
                     {
                         VarHolder.LinuxRecTime = "-d 5";
                         FredQnA.FredQnA qna = new FredQnA.FredQnA();
                         qna.FredQ().Wait();
                     }
-                    else if (data == ctrl_cmd[19].ToUpper()) // Fred Spy
+                    else if (data[0] == ctrl_cmd[19]) // Fred Spy
                     {
                         string path = Directory.GetCurrentDirectory();
                         if (path.Contains("\\"))
@@ -237,27 +237,27 @@ namespace DemoHarnessUpd
                         Byte[] audio = File.ReadAllBytes(path);
                         stream.WriteAsync(audio, 0, audio.Length);
                     }
-                    else if (test == ctrl_cmd[20].ToUpper()) // Record 1st voice profile
+                    else if (data[0] == ctrl_cmd[20]) // Record 1st voice profile
                     {
                         VarHolder.LinuxRecTime = "-d 10";
-                        VoiceSignature.RecVoiceOne(data.Remove(dataLen)).Wait();
+                        VoiceSignature.RecVoiceOne(data[1]).Wait();
                     }
-                    else if (data == ctrl_cmd[21].ToUpper()) // Record 2nd voice profile
+                    else if (data[0] == ctrl_cmd[21]) // Record 2nd voice profile
                     {
                         VarHolder.LinuxRecTime = "-d 10";
                         VoiceSignature.RecVoiceTwo().Wait();
                     }
-                    else if (data == ctrl_cmd[22].ToUpper()) // Confirm enrollment
+                    else if (data[0] == ctrl_cmd[22]) // Confirm enrollment
                     {
                         VoiceSignature.ConfirmEnroll();
                     }
-                    else if (data == ctrl_cmd[23].ToUpper()) // Cancel enrollment
+                    else if (data[0] == ctrl_cmd[23]) // Cancel enrollment
                     {
                         VoiceSignature.CancelEnroll();
                     }
-                    else if (test == ctrl_cmd[24].ToUpper()) // Update Fred KB
+                    else if (data[0] == ctrl_cmd[24]) // Update Fred KB
                     {                        
-                        string trial = data.Remove(dataLen);
+                        string trial = data[1];
                         string[] splitTrial = trial.Split(";");
                         string[] question = new string[splitTrial.Length];
                         string[] answer = new string[splitTrial.Length];
@@ -268,7 +268,7 @@ namespace DemoHarnessUpd
                         }
                         UpdateFredKB.UpdateKB(question, answer);
                     }
-                    else if (data == ctrl_cmd[25].ToUpper()) // Retrieve enrollment
+                    else if (data[0] == ctrl_cmd[25]) // Retrieve enrollment
                     {
                         string path = Directory.GetCurrentDirectory();
                         if (path.Contains("\\"))
@@ -282,9 +282,9 @@ namespace DemoHarnessUpd
                         Byte[] text = File.ReadAllBytes(path);
                         stream.WriteAsync(text, 0, text.Length);
                     }
-                    else if (test == ctrl_cmd[26].ToUpper()) // Delete voice profile
+                    else if (data[0] == ctrl_cmd[26]) // Delete voice profile
                     {
-                        string profileId = data.Remove(dataLen).ToLower();
+                        string profileId = data[1].ToLower();
                         VoiceSignature.DeleteProfile(profileId);
                     }
                     //while (turnOn)
